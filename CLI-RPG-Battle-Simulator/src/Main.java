700c63e2f27c5b95ac4ae14f569cb91589a05bef
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-08-12 19:17:11 +0530

    CLI Menu

--- CLI-RPG-Battle-Simulator/src/Main.java
+++ CLI-RPG-Battle-Simulator/src/Main.java
@@ -1,19 +1,33 @@
-import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Warrior W1 = new Warrior("Thrain", 120, 30);
-        
-        Warrior W2 = new Warrior("Grukk", 130, 12);
-        
-        Mage M1 = new Mage("Nula", 125, 25, 50);
-        
-        Battle.fight(W2, W1);
+        Scanner sc = new Scanner(System.in);
+        Warrior grukk = new Warrior("Grukk", 130, 12);
+        Mage nula = new Mage("Nula", 125, 25, 50);
 
-        SaveManager.saveWarrior(W2, "data.txt");
+        boolean running = true;
+        while (running) {
+            System.out.println("1. Fight\n2. Save Grukk\n3. Load Grukk\n4. Exit");
+            int choice = sc.nextInt();
 
-        Warrior loaded = SaveManager.loadWarrior("data.txt");
-        System.out.println(loaded.getName() + " " + loaded.getHealth() + " " + loaded.getAttackPower());
-    
+            switch (choice) {
+                case 1:
+                    Battle.fight(grukk, nula);
+                    break;
+                case 2:
+                    SaveManager.saveWarrior(grukk, "data.txt");
+                    break;
+                case 3:
+                    Warrior loaded = SaveManager.loadWarrior("data.txt");
+                    System.out.println(loaded.getName() + " " + loaded.getHealth() + " " + loaded.getAttackPower());
+                    break;
+                case 4:
+                    running = false;                            
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
